d6d77f8c5496949bef71e1250b838b0cdf669218
Thomas Günther <mail@medienbaecker.com>
2019-09-02 13:05:53 +0200

    Starting from scratch with a simple field group

--- languages/de.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	'link-field.change'		=> 'Link-Typ ändern',
-	'link-field.email'		=> 'E-Mail',
-	'link-field.file'		=> 'Datei',
-	'link-field.page'		=> 'Seite',
-	'link-field.phone'		=> 'Telefon',
-	'link-field.url'		=> 'URL'
-);
--- languages/en.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	'link-field.change'		=> 'Change link type',
-	'link-field.email'		=> 'Email',
-	'link-field.file'		=> 'File',
-	'link-field.page'		=> 'Page',
-	'link-field.phone'		=> 'Phone',
-	'link-field.url'		=> 'URL'
-);
